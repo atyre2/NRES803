4d2b14882744ae905b60742d761cbab88918f413
Drew Tyre <atyre2@unl.edu>
2020-12-14 11:36:11 -0600

    fix tests

--- tests/testthat/test-augment.R
+++ tests/testthat/test-augment.R
@@ -1,11 +1,11 @@
 x <- mgcv::gam(Sepal.Width ~ s(Petal.Length), data = iris)
 #saveRDS(augment(x), "tests/testthat/test-augment-x.RDS")
-expected_x <- readRDS("tests/testthat/test-augment-x.RDS")
+expected_x <- readRDS(here::here("tests/testthat/test-augment-x.RDS"))
 dat <- mgcv::gamSim(1,n=400,dist="binary",scale=.33)
 lr.fit <- mgcv::gam(y~s(x0)+s(x1)+s(x2)+s(x3),family=binomial,
                data=dat,method="REML")
-saveRDS(augment(lr.fit), "tests/testthat/test-augment-lr.fit.RDS")
-expected_lr.fit <- readRDS("tests/testthat/test-augment-lr.fit.RDS")
+#saveRDS(augment(lr.fit), "tests/testthat/test-augment-lr.fit.RDS")
+expected_lr.fit <- readRDS(here::here("tests/testthat/test-augment-lr.fit.RDS"))
 
 
 
@@ -13,5 +13,5 @@ expected_lr.fit <- readRDS("tests/testthat/test-augment-lr.fit.RDS")
 testthat::test_that("augment.gam works as expected",{
   expect_identical(augment(x), expected_x)
   expect_length(augment(x), 7)
-  expect_s3_class("data.frame")
+  expect_s3_class(augment(x), "data.frame")
 })
